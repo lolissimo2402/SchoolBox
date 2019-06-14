9bc674047e6ad992f8c90927647e1b45adbaf090
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-14 11:35:24 +0200

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+    //1. зчитати з екрану стрічку і вивести на екран її довжину
+namespace System
+{
+    class Program
+    {
+    static void Main(string[] args)
+          
+        {
+            Console.WriteLine("MyFirstTask");
+           String FirstName = "FirstMyTask";
+            Console.WriteLine("FirstName has {0} characters.", FirstName.Length);
+            Console.ReadLine();
+
+        }
+    }
+}
