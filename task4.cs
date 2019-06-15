b17cc664b272cc27d457935bcc2aeeb1250ca381
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-15 17:52:51 +0200

    Add files via upload

---
+++ task4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//4. Зчитати стрічку з екрану, виести її назад у аппер кейсі
+namespace SchoolBox.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string myString = "Superman";
+            string upper = myString.ToUpper();
+            Console.WriteLine(upper);
+
+        }   
+
+
+
+
+
+
+
+    }
+}
