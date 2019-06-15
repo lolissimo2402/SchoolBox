12f96ab3ff0eba92425a987308c344aa1d585586
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-15 19:15:09 +0200

    Add files via upload

---
+++ task5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//. Зчитати число з екрану, використовуючи цикли(for/ foreach/while/do-while) вивести числа від одного до заданого
+namespace SchoolBox.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int a = 1; a < 10; a++ )
+            Console.WriteLine(a);
+
+        }
+    }
+}
