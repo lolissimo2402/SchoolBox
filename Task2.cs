c14e456c0754c61ef9cbed5f53aeb073998e08ec
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-15 14:46:36 +0200

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//2. Зчитати стрічку з екрану, створити метод який вертає стрічку без пробілів
+namespace SchoolBox.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string firstName = "My Second Task";
+            Console.WriteLine(firstName.Replace(" ", ""));
+            Console.ReadLine();
+        }
+    }
+}
