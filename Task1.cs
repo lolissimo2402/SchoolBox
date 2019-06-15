a5fd1a42f9eb509a1a5a46d3aedce1ec08b9bb77
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-15 11:17:01 +0200

    Update Task1.cs

--- Task1.cs
+++ Task1.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-
-
     //1. зчитати з екрану стрічку і вивести на екран її довжину
 namespace SchoolBox.Task1
 {
     class Program
     {
-    static void Main(string[] args)
-          
+    static void Main(string[] args);  
         {
            Console.WriteLine("MyFirstTask");
            String firstName = "FirstMyTask";
            Console.WriteLine("firstName has {0} characters.", firstName.Length);
            Console.ReadLine();
-
         }
     }
 }
