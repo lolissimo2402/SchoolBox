1574c030ddc182949f855e37b65df54f791978c1
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-19 12:06:47 +0200

    Add files via upload

---
+++ task7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SchoolBox.task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string myString;
+            Console.WriteLine("Enter the pass");
+            myString = Console.ReadLine();
+            if (myString.Length < 5)
+                Console.WriteLine("5");
+            else if ((myString.Length >= 5) && (myString.Length <= 10))
+                Console.WriteLine("10");
+            else if (myString.Length > 10)
+                Console.WriteLine(11);
+            Console.ReadLine();
+
+
+            
+
+            
+        }
+    }
+}
