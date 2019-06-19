8b84d3d3269d1261965a737536d11c672e39308e
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-19 10:55:59 +0200

    Add files via upload

---
+++ Task6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//7. Зчитати стрічку з екрану, якщо ввели B - вивести BMW, якщо M - Mersedess, W - Wolzvagen інакше вивести unknown car(використовуй switch -case)
+namespace SchoolBox.task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type of cars: B=BMW, M=Mercedess, W=Wolcwagen");
+            Console.WriteLine("Please enter your selection: ");
+            string myString = Console.ReadLine();
+            switch (myString)
+            {
+                case "B":
+                    Console.WriteLine("BMW");
+                    Console.WriteLine("Thank You");
+                    break;
+                case "M":
+                    Console.WriteLine("Mercedess");
+                    Console.WriteLine("Thank You");
+                    break;
+                case "W":
+                    Console.WriteLine("Wolcvagen");
+                    Console.WriteLine("Thank You");
+                    break;
+                default:
+                    Console.WriteLine("unknown car");
+                    Console.WriteLine("Thank You");
+                    break;
+
+            }
+        }
+        
+    }
+}
