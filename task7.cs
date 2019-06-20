c1a4c822a07ae59c76572081c258a116462cfb30
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-20 10:54:06 +0200

    Update task7.cs
    Console.WriteLine("Hello World!");  на початку вииває завжди..це як заголовок я його просто не видаляю..

--- task7.cs
+++ task7.cs
@@ -10,12 +10,16 @@ namespace SchoolBox.task7
             string myString;
             Console.WriteLine("Enter the pass");
             myString = Console.ReadLine();
+           
             if (myString.Length < 5)
                 Console.WriteLine("5");
+           
             else if ((myString.Length >= 5) && (myString.Length <= 10))
                 Console.WriteLine("10");
+           
             else if (myString.Length > 10)
                 Console.WriteLine(11);
+            
             Console.ReadLine();          
         }
     }
